ab34b5477db80045e9510c9e10cbe62c093b8289
Mahmoud Gamie <mimg414@gmail.com>
2018-08-20 17:35:41 +0200

    feat: loaded the google api map

--- src/App.js
+++ src/App.js
@@ -3,19 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount(){
+    this.loadMap();
+  }
+
+  loadMap = () => {
+    const url = 'https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDo4mqXNW4jwA4PdjxZx2w7qPumf865ATc&callback=initMap';
+    loadScript(url);
+    window.initMap = this.initMap;
+  }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main>
+        <div id='map'></div>
+      </main>
     );
   }
+}
+
+function loadScript(url){
+  const firstScriptTag = window.document.getElementsByTagName('script')[0];
+  const mapScript = window.document.createElement('script');
+  mapScript.src = url;
+  mapScript.async = true;
+  mapScript.defer = true;
+  firstScriptTag.parentNode.insertBefore(mapScript, firstScriptTag);
 }
 
 export default App;
