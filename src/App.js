920780c36cc64e202ff4f6eb961f86132cf13095
Mahmoud Gamie <mimg414@gmail.com>
2018-08-21 22:43:50 +0200

    feat: retrieving all locations from foursquare api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
 
-  componentDidMount(){
-    this.loadMap();
+  state = {
+    venues: ''
+  }
+
+  componentDidMount() {
+    this.getVenues();
   }
 
   loadMap = () => {
@@ -14,11 +18,34 @@ class App extends Component {
     window.initMap = this.initMap;
   }
 
+  getVenues = () => {
+    const url = 'https://api.foursquare.com/v2/venues/explore?';
+    const parameters = {
+      client_id: '5ZEYADCII2MVM00YFX3ZPRR3SEXKAPVO5IAV2DO1ZAYJ04L3',
+      client_secret: 'LUSQFIHZMYTTVXNWDYV0351HAXOG0W5WFJGYZ22PYNILGZZN',
+      query: 'shops',
+      near: 'Cairo',
+      v: '20182507'
+    };
+    axios.get(url + new URLSearchParams(parameters))
+      .then(response => this.setState({ venues: response.data.response.groups[0].items }, this.loadMap()))
+      .catch(error => console.log('Error: ' + error));
+  }
+
   initMap = () => {
+    const myCity = { lat: 30.06263, lng: 31.24967 };
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8
+      center: myCity,
+      zoom: 12
     });
+
+    this.state.venues.map(ele => {
+      const marker = new window.google.maps.Marker({
+        position: { lat: ele.venue.location.lat, lng: ele.venue.location.lng },
+        map: map,
+        title: ele.venue.name
+      });
+    })
   }
   render() {
     return (
@@ -29,9 +56,9 @@ class App extends Component {
   }
 }
 
-function loadScript(url){
-  const firstScriptTag = window.document.getElementsByTagName('script')[0];
-  const mapScript = window.document.createElement('script');
+function loadScript(url) {
+  const firstScriptTag = document.getElementsByTagName('script')[0];
+  const mapScript = document.createElement('script');
   mapScript.src = url;
   mapScript.async = true;
   mapScript.defer = true;
